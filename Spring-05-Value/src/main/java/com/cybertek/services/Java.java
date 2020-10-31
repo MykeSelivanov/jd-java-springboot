bf637973946c6391a4f1465463d7386761bc119f
MykeSelivanov <michael777selivanov@gmail.com>
2020-10-30 21:56:07 -0500

    Value

---
+++ Spring-05-Value/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,45 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class Java implements Course {
+
+    @Value("JD1")
+    private String batch;
+    @Value("${instructor}")
+    private String instructor;
+    @Value("${days}")
+    private String[] days;
+
+    //@Autowired
+    //@Qualifier("officeHours")
+    private ExtraSessions extraSessions;
+
+    @Autowired
+    public Java(@Qualifier("officeHours") ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Java teaching hours: " + (30 + extraSessions.getHours()));
+    }
+
+    @Override
+    public String toString() {
+        return "Java{" +
+                "batch='" + batch + '\'' +
+                ", instructor='" + instructor + '\'' +
+                ", days=" + Arrays.toString(days) +
+                '}';
+
+    }
+}
