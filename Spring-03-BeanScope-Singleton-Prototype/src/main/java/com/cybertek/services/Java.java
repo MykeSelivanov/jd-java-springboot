91aab512e5e70d380006d5fd4165a965108e3517
MykeSelivanov <michael777selivanov@gmail.com>
2020-10-30 21:38:12 -0500

    BeanScope Singleton Prototype

---
+++ Spring-03-BeanScope-Singleton-Prototype/src/main/java/com/cybertek/services/Java.java
@@ -0,0 +1,14 @@
+package com.cybertek.services;
+
+import com.cybertek.interfaces.Course;
+
+public class Java implements Course {
+
+
+
+    @Override
+    public void getTeachingHours() {
+
+        System.out.println("Weekly Teaching hours: ");
+    }
+}
