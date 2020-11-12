ac4484db5e028fcafc9713a9556c1552e8cdf0c4
MykeSelivanov <michael777selivanov@gmail.com>
2020-11-12 14:00:00 +0100

    Spring Container

--- Spring-01-SpringContainer/src/main/java/services/FullTimeMentor.java
+++ Spring-01-SpringContainer/src/main/java/services/FullTimeMentor.java
@@ -5,6 +5,7 @@ import interfaces.Mentor;
 public class FullTimeMentor implements Mentor {
     @Override
     public void createAccount() {
+
         System.out.println("Full Time Mentor Account is created");
     }
 }
--- Spring-01-SpringContainer/src/main/java/services/PartTimeMentor.java
+++ Spring-01-SpringContainer/src/main/java/services/PartTimeMentor.java
@@ -5,6 +5,7 @@ import interfaces.Mentor;
 public class PartTimeMentor implements Mentor {
     @Override
     public void createAccount() {
+
         System.out.println("Part Time Mentor Account is created");
     }
 }
