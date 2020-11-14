74ba7fda9761078fc429fd1d6f594e2d5249689d
MykeSelivanov <michael777selivanov@gmail.com>
2020-11-14 10:55:50 -0600

    MVC Model View (Thymeleaf)

---
+++ Spring-08-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/cybertek/controller/StudentController.java
@@ -0,0 +1,45 @@
+package com.cybertek.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+    @GetMapping("/welcome")
+    public String homePage(Model model){
+
+        model.addAttribute("name", "Mike");
+        model.addAttribute("course", "Thymeleaf");
+
+        String subject = "Collections";
+        model.addAttribute("subject", subject);
+
+        //create some random studentId (0-1000)
+        int studentId = new Random().nextInt(1000);
+        model.addAttribute("studentId", studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        model.addAttribute("numbers", numbers);
+
+        //print your birthday
+        LocalDate birthday = LocalDate.now().minusYears(29);
+        model.addAttribute("birthday", birthday);
+
+        return "student/welcome";
+    }
+
+}
