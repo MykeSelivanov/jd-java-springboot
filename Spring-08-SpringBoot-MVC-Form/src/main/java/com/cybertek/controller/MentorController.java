9df13b1d598b673d14ba14abcd686750ff87eaac
MykeSelivanov <michael777selivanov@gmail.com>
2020-11-17 22:30:13 -0600

    SpringBoot - MVC - Form

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/MentorController.java
@@ -4,6 +4,8 @@ import com.cybertek.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Arrays;
@@ -22,7 +24,14 @@ public class MentorController {
         model.addAttribute("batchList", batchList);
 
         return "mentor/mentor-register";
+    }
 
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor") Mentor mentor){
+
+        System.out.println(mentor.toString());
+
+        return "mentor/mentor-confirmation";
     }
 
 }
