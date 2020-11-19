16c87d8e97c550fb7f7d2683385140cb860e4429
MykeSelivanov <michael777selivanov@gmail.com>
2020-11-18 18:50:38 -0600

    SpringBoot - MVC - Form

---
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.cybertek.controller;
+
+import com.cybertek.datagenerator.DataGenerator;
+import com.cybertek.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @GetMapping("/register")
+    public String employeeCreate(Model model){
+
+        model.addAttribute("employee", new Employee());
+        model.addAttribute("stateList", DataGenerator.getStateList());
+
+        return "employee/employee-create";
+    }
+
+}
