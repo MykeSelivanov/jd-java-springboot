ebdb0ab622e54537dbc466f267496a5550091175
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-12 12:37:20 -0600

    ORM Table Creation

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Car.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Car.java
@@ -2,8 +2,10 @@ package com.cybertek.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="cars")
 public class Car {
 
     @Id
---
+++ Spring-09-ORM-TableCreation/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO student(id,first_name,last_name,email) VALUES(1,'Mike','Smith','mike@cybertek.com');
+INSERT INTO student(id,first_name,last_name,email) VALUES(2,'Adam','Shaun','adam@cybertek.com');
+INSERT INTO student(id,first_name,last_name,email) VALUES(3,'Brad','Pitt','brad@cybertek.com');
+INSERT INTO student(id,first_name,last_name,email) VALUES(4,'Tom','Hanks','tom@cybertek.com');
