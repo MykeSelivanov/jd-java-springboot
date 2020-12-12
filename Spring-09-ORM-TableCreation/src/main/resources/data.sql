8af44e6286b75a6251598d38f48124f698bcdbf8
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-12 14:07:12 -0600

    ORM Table Creation

---
+++ Spring-09-ORM-TableCreation/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO students(student_first_name,student_last_name,student_email_address) VALUES('Mike','Smith','mike@cybertek.com');
+INSERT INTO students(student_first_name,student_last_name,student_email_address) VALUES('Adam','Shaun','adam@cybertek.com');
+INSERT INTO students(student_first_name,student_last_name,student_email_address) VALUES('Brad','Pitt','brad@cybertek.com');
+INSERT INTO students(student_first_name,student_last_name,student_email_address) VALUES('Tom','Hanks','tom@cybertek.com');
--- Spring-09-ORM-TableCreation/src/main/resources/data2.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO students(student_first_name,student_last_name,student_email_address) VALUES(1,'Mike','Smith','mike@cybertek.com');
-INSERT INTO students(student_first_name,student_last_name,student_email_address) VALUES(2,'Adam','Shaun','adam@cybertek.com');
-INSERT INTO students(student_first_name,student_last_name,student_email_address) VALUES(3,'Brad','Pitt','brad@cybertek.com');
-INSERT INTO students(student_first_name,student_last_name,student_email_address) VALUES(4,'Tom','Hanks','tom@cybertek.com');
