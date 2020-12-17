9c746d17424cedf74a193cc5bba38c14c8e719f3
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-16 18:32:20 -0600

    ORM-Mapping-OneToMany

---
+++ Spring-09-ORM-OneToMany/src/main/java/com/ormonetomany/entity/Address.java
@@ -0,0 +1,19 @@
+package com.ormonetomany.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Address {
+
+    @Id
+    private Long id;
+
+    private String street;
+    private String zipCode;
+
+    @ManyToOne
+    private Person person;
+
+}
