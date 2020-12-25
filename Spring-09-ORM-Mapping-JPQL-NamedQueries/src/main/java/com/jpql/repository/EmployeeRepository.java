eee8a10db5525571ad0d3ee8a44728687127d962
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-24 18:55:31 -0600

    ORM-JPQL-NamedQueries

---
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/jpql/repository/EmployeeRepository.java
@@ -0,0 +1,21 @@
+package com.jpql.repository;
+
+import com.jpql.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+    @Query("SELECT e FROM Employee e WHERE e.email = 'dtrail8@tamu.edu'")
+    Employee getEmployeeDetail();
+
+    @Query("SELECT e.salary FROM Employee e WHERE e.email = 'dtrail8@tamu.edu'")
+    BigDecimal getEmployeeSalary();
+
+
+
+}
