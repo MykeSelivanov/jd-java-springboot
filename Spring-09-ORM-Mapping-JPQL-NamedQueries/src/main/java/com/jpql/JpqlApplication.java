84dca8c8dda5aa1b3df38b48920b680f8dac9e0c
MykeSelivanov <michael777selivanov@gmail.com>
2020-12-24 18:58:27 -0600

    ORM-JPQL-NamedQueries

--- Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/jpql/JpqlApplication.java
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/jpql/JpqlApplication.java
@@ -1,13 +1,29 @@
 package com.jpql;
 
+import com.jpql.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class JpqlApplication {
 
+	@Autowired
+	EmployeeRepository employeeRepository;
+
 	public static void main(String[] args) {
-		SpringApplication.run(JpqlApplication.class, args);
-	}
+		SpringApplication.run(JpqlApplication.class, args); }
+
+		@PostConstruct
+		public void testEmployee(){
+
+			System.out.println(employeeRepository.getEmployeeDetail());
+			System.out.println(employeeRepository.getEmployeeSalary());
+
+		}
+
+
 
 }
