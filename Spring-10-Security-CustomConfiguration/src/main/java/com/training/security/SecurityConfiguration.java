e8d939fcf9c375a101da8d50038387720cfb64d1
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-14 10:21:56 -0600

    Security - Custom Configuration

---
+++ Spring-10-Security-CustomConfiguration/src/main/java/com/training/security/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.training.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests() //request should be authorized
+                .anyRequest().authenticated() //incoming request should be authenticated
+                .and()
+                .httpBasic(); //perform basic http authentication
+    }
+
+
+
+}
