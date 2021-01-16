969e52d1c0951a9f19b181f0dc97649ff7450c8c
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-16 13:52:43 -0600

    Security - HttpBasic Database

--- Spring-10-Security-HttpBasic-Database/src/main/java/com/training/bootstrap/DataGenerator.java
+++ Spring-10-Security-HttpBasic-Database/src/main/java/com/training/bootstrap/DataGenerator.java
@@ -4,17 +4,24 @@ import com.training.entity.User;
 import com.training.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
 
+@Service
 public class DataGenerator implements CommandLineRunner {
 
     private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public DataGenerator(UserRepository userRepository) {
+    public DataGenerator(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -24,13 +31,12 @@ public class DataGenerator implements CommandLineRunner {
         this.userRepository.deleteAll();
 
         //Create users
-        User employee = new User("mike","mike123","USER","");
-        User admin = new User("admin","admin123","ADMIN","ACCESS_TEST1,ACCESS_TEST2");
-        User manager = new User("manager","manager123","MANGER","ACCESS_TEST1");
+        User employee = new User("mike",passwordEncoder.encode("mike123"),"USER","");
+        User admin = new User("admin",passwordEncoder.encode("admin123"),"ADMIN","ACCESS_TEST1,ACCESS_TEST2");
+        User manager = new User("manager",passwordEncoder.encode("manager123"),"MANAGER","ACCESS_TEST1");
 
         List<User> users = Arrays.asList(employee,admin,manager);
         userRepository.saveAll(users);
-
     }
 
 }
---
+++ Spring-10-Security-HttpBasic-Database/src/main/java/com/training/security/UserPrincipalDetailsService.java
@@ -0,0 +1,29 @@
+package com.training.security;
+
+import com.training.entity.User;
+import com.training.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserPrincipalDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserPrincipalDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(s);
+        UserPrincipal userPrincipal = new UserPrincipal(user);
+
+        return userPrincipal;
+    }
+
+}
