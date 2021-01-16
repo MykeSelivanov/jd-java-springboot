1666df57859169777b59dfc338053a3bfea4eff9
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-16 12:43:23 -0600

    Security - HttpBasic Database

---
+++ Spring-10-Security-HttpBasic-Database/src/main/java/com/training/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.training.repository;
+
+import com.training.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    User findByUsername(String username);
+
+}
