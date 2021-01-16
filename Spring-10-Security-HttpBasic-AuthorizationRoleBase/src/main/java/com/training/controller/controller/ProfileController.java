f5114631da03e021f6eb0465f60c28ac323a5df0
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-16 12:16:59 -0600

    Security - HttpBasic Authorization RoleBase

--- Spring-10-Security-HttpBasic-AuthorizationRoleBase/src/main/java/com/training/controller/controller/ProfileController.java
+++ Spring-10-Security-HttpBasic-AuthorizationRoleBase/src/main/java/com/training/controller/controller/ProfileController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/management")
+@RequestMapping("/profile")
 public class ProfileController {
 
     @GetMapping("/index")
