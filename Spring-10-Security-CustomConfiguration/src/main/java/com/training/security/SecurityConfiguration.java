6567684400640cacc12ad81649dbc4a56be40b40
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-16 10:33:37 -0600

    Security - HttpBasic Authorization RoleBase

--- Spring-10-Security-CustomConfiguration/src/main/java/com/training/security/SecurityConfiguration.java
+++ Spring-10-Security-CustomConfiguration/src/main/java/com/training/security/SecurityConfiguration.java
@@ -17,10 +17,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests() //request should be authorized
-                .antMatchers("index.html").permitAll()
-                .antMatchers("/profile/**").authenticated()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/management/**").hasAnyRole("ADMIN","MANAGER")
+                .anyRequest().authenticated() //incoming request should be authenticated
                 .and()
                 .httpBasic(); //perform basic http authentication
     }
@@ -31,9 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .inMemoryAuthentication()
                 .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
                 .and()
-                .withUser("mike").password(passwordEncoder().encode("mike123")).roles("USER")
-                .and()
-                .withUser("manager").password(passwordEncoder().encode("manager123")).roles("MANAGER");
+                .withUser("mike").password(passwordEncoder().encode("mike123")).roles("USER");
     }
 
     @Bean
--- Spring-10-Security-HttpBasic-AuthorizationRoleBase/src/main/java/com/training/controller/controller/HomeController.java
+++ Spring-10-Security-HttpBasic-AuthorizationRoleBase/src/main/java/com/training/controller/controller/HomeController.java
@@ -1,16 +1,15 @@
-package com.training.controller;
+package com.training.controller.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/management")
-public class ManagementController {
+@RequestMapping("/")
+public class HomeController {
 
-    @GetMapping("/index")
+    @GetMapping("index")
     public String index(){
-        return "management/index";
+        return "/index";
     }
-
 }
