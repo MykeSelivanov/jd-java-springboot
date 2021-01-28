3f64b598a6630af62014da00447d37838eea0fa1
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-27 19:04:16 -0600

    Spring REST - Rest Template

---
+++ Spring-11-Rest-RestTemplate/src/main/java/com/training/controller/HomeController.java
@@ -0,0 +1,51 @@
+package com.training.controller;
+
+import com.training.entity.User;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+
+@RestController
+public class HomeController {
+
+    final String URI = "https://jsonplaceholder.typicode.com/users";
+
+    private RestTemplate restTemplate;
+
+    public HomeController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping
+    public User[] readAllUsers() {
+        ResponseEntity<User[]> responseEntity = restTemplate.getForEntity(URI, User[].class);
+        return responseEntity.getBody();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Object readUser(@PathVariable("id") Integer id){
+        String URL = URI + "/{id}";
+
+        return restTemplate.getForObject(URL, Object.class, id);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<Object> consumePostFromDummyApi(){
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.set("app-id","lTE5abbDxdjGplutvTuc");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Object> response = restTemplate.exchange("https://dummyapi.io/data/api/user?limit=10", HttpMethod.GET, entity, Object.class);
+
+        return response;
+    }
+
+}
