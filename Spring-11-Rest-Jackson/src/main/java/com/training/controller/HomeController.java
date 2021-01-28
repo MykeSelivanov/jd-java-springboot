bbc416f3982f801f224206ce16500b9bbb11a67d
MykeSelivanov <michael777selivanov@gmail.com>
2021-01-27 18:06:13 -0600

    Spring REST - Jackson

--- Spring-11-Rest-Jackson/src/main/java/com/training/controller/HomeController.java
+++ Spring-11-Rest-Jackson/src/main/java/com/training/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.training.controller;
 
+import com.training.entity.Account;
 import com.training.entity.User;
+import com.training.repository.AccountRepository;
 import com.training.repository.UserRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +13,22 @@ import java.util.List;
 public class HomeController {
 
     private UserRepository userRepository;
+    private AccountRepository accountRepository;
 
-    public HomeController(UserRepository userRepository) {
+    public HomeController(UserRepository userRepository, AccountRepository accountRepository) {
         this.userRepository = userRepository;
+        this.accountRepository = accountRepository;
     }
 
     @GetMapping("/users")
     public List<User> readAllUsers(){
         return userRepository.findAll();
+    }
+
+    @GetMapping("/accounts")
+    public List<Account> readAllAccounts(){
+
+        return accountRepository.findAll();
     }
 
 }
