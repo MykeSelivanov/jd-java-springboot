56b6211578de45999ae599b28cfb78f5470b5a8f
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-10 22:20:32 -0600

    Spring REST - Security JWT

--- Spring-11-Rest-Security-JWT/src/main/java/com/training/config/WebSecurityConfig.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/training/config/WebSecurityConfig.java
@@ -23,13 +23,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return super.authenticationManagerBean();
     }
 
+    private static final String[] permittedUrls = {
+            "/authenticate",
+            "/create-user",
+            "/api/p1/**",
+            "/v3/api-docs/**",
+            "/swagger/resources/**",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/webjars/**"
+    };
+
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/authenticate","/create-user")
+                .antMatchers(permittedUrls)
                 .permitAll()
                 .anyRequest()
                 .authenticated();
--- Spring-11-Rest-Security-JWT/src/main/java/com/training/controller/AuthenticationController.java
+++ Spring-11-Rest-Security-JWT/src/main/java/com/training/controller/AuthenticationController.java
@@ -7,6 +7,8 @@ import com.training.entity.User;
 import com.training.exception.ServiceException;
 import com.training.service.UserService;
 import com.training.util.JWTUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Authenticate controller", description = "Authenticate API")
 public class AuthenticationController {
 
     private UserService userService;
@@ -29,6 +32,7 @@ public class AuthenticationController {
 
     @PostMapping("/authenticate")
     @DefaultExceptionMessage(defaultMessage = "Bad Credentials")
+    @Operation(summary = "Login to application")
     public ResponseEntity<ResponseWrapper> doLogin(@RequestBody AuthenticationRequest authenticationRequest){
 
         String password = authenticationRequest.getPassword();
@@ -47,6 +51,7 @@ public class AuthenticationController {
 
     @PostMapping("/create-user")
     @DefaultExceptionMessage(defaultMessage = "Failed to create user, please try again")
+    @Operation(summary = "Create new user")
     public ResponseEntity<ResponseWrapper> createAccount(@RequestBody User user) throws ServiceException {
         User createdUser = userService.createUser(user);
         return ResponseEntity.ok(new ResponseWrapper("User has been created successfully", createdUser));
