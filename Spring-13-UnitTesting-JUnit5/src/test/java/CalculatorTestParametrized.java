9da771ef7caa17f46e6d8dd2f07187cf52d029bf
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-27 12:24:49 -0600

    Spring Unit Testing

---
+++ Spring-13-UnitTesting-JUnit5/src/test/java/CalculatorTestParametrized.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class CalculatorTestParametrized {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Java","JS","TS"})
+    void testCase1(String args){
+        Assertions.assertTrue(!args.isEmpty());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 6, 9})
+    void testCase2(int number){
+        Assertions.assertEquals(0, number % 3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"Java", "JS", "TS"})
+//    @EmptySource
+//    @NullSource
+    @NullAndEmptySource
+    void testCase3(String args){
+        Assertions.assertTrue(!args.isEmpty());
+    }
+
+}
