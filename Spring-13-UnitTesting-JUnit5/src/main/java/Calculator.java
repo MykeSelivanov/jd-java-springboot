a76ca9d8b93c9be15bab3a4f6c33599e88c869af
MykeSelivanov <michael777selivanov@gmail.com>
2021-02-27 10:27:39 -0600

    Spring Unit Testing

--- Spring-13-UnitTesting-JUnit5/src/main/java/Calculator.java
+++ Spring-13-UnitTesting-JUnit5/src/main/java/Calculator.java
@@ -1,7 +1,10 @@
 public class Calculator {
 
+    public static String operator = "add";
+
     public static int add(int num1, int num2){
         return num1 + num2;
     }
+
 
 }
--- Spring-13-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
+++ Spring-13-UnitTesting-JUnit5/src/test/java/CalculatorTest.java
@@ -6,7 +6,48 @@ class CalculatorTest {
 
     @Test
     void add(){
-
+        int actual = Calculator.add(2,3);
+        assertEquals(5,actual);
     }
+
+    @Test
+    void testCase1(){
+//        fail("Not implemented yet");
+    }
+
+    @Test
+    void testCase2(){
+        assertEquals(20,20);
+    }
+
+    @Test
+    void testCase3(){
+       assertTrue(Calculator.operator.equals("add"));
+    }
+
+    @Test
+    void testCase4(){
+        assertArrayEquals(new int[]{1,2,3}, new int[]{1,2,3});
+    }
+
+    @Test
+    void testCase5(){
+        String nullString = null;
+        String notNullString = "Testing";
+
+        assertNotNull(notNullString);
+        assertNull(nullString);
+    }
+
+    @Test
+    void testCase6(){
+        Calculator c1 = new Calculator();
+        Calculator c2 = c1;
+        Calculator c3 = new Calculator();
+
+        assertNotSame(c1,c3);
+        assertSame(c1,c2);
+    }
+
 
 }
